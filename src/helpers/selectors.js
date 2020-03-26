b6457c0a53d4187a7eeb0ce41df43e6a0add4c69
annetta <annetta@email.com>
2020-03-25 17:18:33 -0700

    config for test db, navigation.spec.js cypress tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,9 +4,9 @@ export function getAppointmentsForDay(state, day) {
   const appointmentsForGivenDay = [];
 
   if (state.days.length > 0) {
-    state.days.forEach(days => {
-      if (days.name === day) {
-        const appointments = days.appointments;
+    state.days.forEach(someDay => {
+      if (someDay.name === day) {
+        const appointments = someDay.appointments;
         const allAppointments = Object.keys(state.appointments);
         appointments.forEach(elem => {
           elem = elem.toString();
@@ -22,13 +22,21 @@ export function getAppointmentsForDay(state, day) {
 
 export function getInterview(state, interview) {
     const result = {...interview};
-    if (interview !== null){
+    if (interview !== null) {
       result.interviewer = state.interviewers[interview.interviewer];
     } else {
       return null;
     }
     return result;
   }
+//-------- second version --------
+  // function getInterview(state, interview) {
+  //   if (interview != null) {
+  //     return {...interview, interviewer: {...state.interviewers[interview.interviewer]}};
+  //   } else {
+  //     return null;
+  //   }
+  // }
 
   export function getInterviewersForDay(state, day) {
 
